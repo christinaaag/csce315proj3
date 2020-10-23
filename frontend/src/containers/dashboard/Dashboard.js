020d4138ab6b6ff06c706ca8b050b1156d77c79c
sabrinalu999 <sabrinalu999@tamu.edu>
2020-10-22 22:01:19 -0500

    added sentiment display buttons

--- frontend/src/containers/dashboard/Dashboard.js
+++ frontend/src/containers/dashboard/Dashboard.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Dashboard.css';
+import './../generalStyle.css';
+import logo from './../../assets/logos - white.png';
+import SentDisp from './components/SentDisp';
 
 const Dashboard = () => {
     return (
-        <p>
-            "This is the dashboard!"
-        </p>
+        <div className="body">
+            <Container >
+                <h1 className="title">Moodia<img className="logo" src={logo} alt="" /></h1>
+            </Container>
+            <Container>
+                    <SentDisp/>
+            
+                
+            </Container>
+          
+        </div>
     )
 }
 
