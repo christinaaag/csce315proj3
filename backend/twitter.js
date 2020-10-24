bfef3d173749c5dc2c1729bd24920ef78e7f604d
kn395349 <kn395349@tamu.edu>
2020-10-24 13:24:24 -0500

    added comments to explain process

--- backend/twitter.js
+++ backend/twitter.js
@@ -3,14 +3,13 @@ var Twitter = require('twitter-lite')
 const prompt = require('prompt-async')
 require ('custom-env').env('twitter')
 
-
-
 const client = new Twitter({
     consumer_key: process.env.API_KEY,
     consumer_secret: process.env.API_SECRET
 });
 
 async function oauth() {
+    // gets request token to create authorize url
     var cb_confirm, request_token;
     await client.getRequestToken("oob")
       .then(res =>
@@ -25,15 +24,20 @@ async function oauth() {
         }
       ).catch(console.error);
     
+
+      //user clicks the authorize url, logs in and generates PIN/redirect callback url
       console.log("Click link below to authenticate user for site.")
-      console.log("https://api.twitter.com/oauth/authenticate?oauth_token="+request_token)
+      console.log("https://api.twitter.com/oauth/authorize?oauth_token="+request_token)
+      
+
       console.log("Enter PIN below after login")
-      var one = 1
       prompt.start();
       var oauth_verifier = await prompt.get(['PIN']);
       oauth_verifier = Object.values(oauth_verifier)[0]
       console.log(oauth_verifier)
     
+
+    //after getting PIN/oauth_verifier, gets oauth token of user
     var oauth_token, oauth_token_secret;
     await client.getAccessToken({
         oauth_verifier: oauth_verifier,
