12f97df8678c34d453a3eee0a0308e2f404bea0c
Cole Downey <cdowneymm@gmail.com>
2020-10-24 15:58:51 -0500

    created timeline

--- frontend/src/containers/dashboard/Dashboard.js
+++ frontend/src/containers/dashboard/Dashboard.js
@@ -4,35 +4,38 @@ import { Container, Row, Col } from 'react-bootstrap';
 import './Dashboard.css';
 import './../generalStyle.css';
 import logo from './../../assets/logos - white.png';
+import Timeline from '../../components/Timeline';
 import SentDisp from './components/SentDisp';
 import UserDisp from './components/UserDisp';
 
 const Dashboard = () => {
     return (
         <div className="body">
-            <Container style={{margin: 0}}>
+            <Container style={{ margin: 0 }}>
                 <h1 className="logo">Moodia<img className="logo" src={logo} alt="" /></h1>
             </Container>
-            <Container style={{margin: 0}}>
+            <Container style={{ margin: 0 }}>
 
                 <Col>
 
-                    <UserDisp/>
-             
-                    <SentDisp/>
-                
+                    <UserDisp />
+
+                    <SentDisp />
+
                 </Col>
-                
-                
+
+
 
 
                 <Col>
                     <h2>Twitter Feed</h2>
+                    <br />
+                    <Timeline />
                 </Col>
-                    
-        
+
+
             </Container>
-          
+
         </div>
     )
 }
